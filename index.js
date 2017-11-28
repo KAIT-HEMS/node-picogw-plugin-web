a75592d90e2e6479c2fddb8ffbeb092453173881
Satoshi Ikeda <ikeda@koozyt.com>
2017-11-28 22:28:10 +0900

    Implement client plugin

--- index.js
+++ index.js
@@ -6,22 +6,14 @@ const bodyParser = require('body-parser');
 var mime = require('mime') ;
 var fs = require('fs');
 var pathm = require('path');
-const MyLocalStorage = require('../../../MyLocalStorage.js') ;
-const QuotaLocalStorage = MyLocalStorage.QuotaLocalStorage ;
-const localStorage = new QuotaLocalStorage(pathm.dirname(__filename)+'/localstorage') ;
 
-var clientInterface , globals , almightyClientInterface;
+exports.init = function(pluginInterface){
+    const pi = pluginInterface;
+    const localStorage = pi.localStorage;
+	const log = pi.log;
 
-var log = (msg) => { console.log('client> '+msg); };
-
-exports.init = function(_clientInterface,_globals,_almightyClientInterface){
-	clientInterface = _clientInterface ;
-	globals = _globals ;
-	almightyClientInterface = _almightyClientInterface ;
-
-	log = clientInterface.log ;
-
-	var SERVER_PORT = globals.cmd_opts.get('port') || localStorage.getItem('PORT_NUM') || 8080 ;
+    // TODO: We will replace it with the command line argument function in the future
+	var SERVER_PORT = pi.cmd_opts.get('port') || localStorage.getItem('PORT_NUM') || 8080 ;
 
 	var http = express() ;
 	http.use(bodyParser.urlencoded({ extended: true }));
@@ -40,37 +32,35 @@ exports.init = function(_clientInterface,_globals,_almightyClientInterface){
 		}) ;
 
 		// REST API call
-		globals.VERSIONS.forEach(VERSION=>{
-			http.all(`/${VERSION}/*`, function(req, res, next){
-				// for( var e in req ){if( typeof req[e] == 'string') log(e+':'+req[e]);}
-				// var caller_ip = req.ip ;
-				var args = req.body ;
-				// Overwrite args in body with GET parameters
-				if( req.originalUrl.indexOf('?') >= 0 ){
-					req.originalUrl.slice(req.originalUrl.indexOf('?')+1).split('&').forEach(eq=>{
-						var terms = eq.split('=') ;
-						if( terms[0] == 'callback' || terms[0] == 'jsoncallback' )
-							return ;
-						if( terms.length == 1 ) args.value = decodeURIComponent(terms[0]) ;
-						else					args[terms[0]] = decodeURIComponent(terms[1]) ;
-					}) ;
-				}
+		http.all(`/v.+/*`, function(req, res, next){
+			// for( var e in req ){if( typeof req[e] == 'string') log(e+':'+req[e]);}
+			// var caller_ip = req.ip ;
+			var args = req.body ;
+			// Overwrite args in body with GET parameters
+			if( req.originalUrl.indexOf('?') >= 0 ){
+				req.originalUrl.slice(req.originalUrl.indexOf('?')+1).split('&').forEach(eq=>{
+					var terms = eq.split('=') ;
+					if( terms[0] == 'callback' || terms[0] == 'jsoncallback' )
+						return ;
+					if( terms.length == 1 ) args.value = decodeURIComponent(terms[0]) ;
+					else					args[terms[0]] = decodeURIComponent(terms[1]) ;
+				}) ;
+			}
 
-				// 多分常に文字列。JSONオブジェクトに変換できるときはオブジェクトに、数値に変換できる
-				// 時は数値に、それ以外はそのまま文字列として、プラグインに与える。
-				for( let k in args ){
-					if( typeof args[k] == 'string' ){
-						try {
-							args[k] = JSON.parse(args[k]);
-						} catch(e){
-							if( isFinite(parseInt(args[k])) )	args[k] = parseInt(args[k]) ;
-						}
+			// 多分常に文字列。JSONオブジェクトに変換できるときはオブジェクトに、数値に変換できる
+			// 時は数値に、それ以外はそのまま文字列として、プラグインに与える。
+			for( let k in args ){
+				if( typeof args[k] == 'string' ){
+					try {
+						args[k] = JSON.parse(args[k]);
+					} catch(e){
+						if( isFinite(parseInt(args[k])) )	args[k] = parseInt(args[k]) ;
 					}
 				}
-				clientInterface.callproc({method:req.method,path:req.path,args:args})
-					.then( re=>{res.jsonp(re);} ).catch( re=>{res.jsonp(re);} /*console.error*/ ) ;
+			}
+			pi.client.callProc({method:req.method,path:req.path,args:args})
+				.then( re=>{res.jsonp(re);} ).catch( re=>{res.jsonp(re);} /*console.error*/ ) ;
 
-			}) ;
 		}) ;
 
 		// Static contents call
@@ -84,10 +74,10 @@ exports.init = function(_clientInterface,_globals,_almightyClientInterface){
 					return ;
 				}
 				if(path==='/index.html'){
-					data = data.toString().split('__%%RSA_PUB_KEY%%__').join('"'+globals.getPubKey()+'"')
+					data = data.toString().split('__%%RSA_PUB_KEY%%__').join('"'+pi.crypt.getPubKey()+'"')
 						.split('__%%ADDITIONAL_LICENSES%%__').join('""') ;
 				}
-				res.set('Content-Type', mime.lookup(path) /*'text/html; charset=UTF-8'*/);
+				res.set('Content-Type', mime.getType(path) /*'text/html; charset=UTF-8'*/);
 				res.status(200);
 
 				res.send(data) ;
@@ -122,41 +112,42 @@ exports.init = function(_clientInterface,_globals,_almightyClientInterface){
 
 		    //log((new Date()) + ' Connection accepted.');
 		    let subscribe_funcs = {} ;
-		    connection.on('message', function(message) {
-		        if (message.type === 'utf8') {
-					//log('Received Message: ' + message.utf8Data);
+			connection.on('message', function(message) {
+				if (message.type === 'utf8') {
+					log('Received Message: ' + message.utf8Data);
 					let req ;
-		        	try {
-		        		req = JSON.parse(message.utf8Data) ;
-		        		if( req.method.toUpperCase() == 'SUB' ){
-		        			let cbfunc = function(re){
+					try {
+						req = JSON.parse(message.utf8Data) ;
+						if( req.method.toUpperCase() == 'SUB' ){
+							let cbfunc = function(re){
 								connection.sendUTF(JSON.stringify(re)) ;
 							} ;
 							if( subscribe_funcs[req.path] == undefined ){
-								almightyClientInterface.subscribe(req.path,cbfunc) ;
+								pi.client.subscribe(req.path,cbfunc) ;
 								subscribe_funcs[req.path] = cbfunc ;
 							}
 							connection.sendUTF(JSON.stringify({success:true,tid:req.tid}));
-		        		} else if( req.method.toUpperCase() == 'UNSUB' ){
-		        			if( subscribe_funcs[req.path] != undefined ){
-								almightyClientInterface.unsubscribe(req.path,subscribe_funcs[req.path]) ;
+						} else if( req.method.toUpperCase() == 'UNSUB' ){
+							if( subscribe_funcs[req.path] != undefined ){
+								pi.client.unsubscribe(req.path,subscribe_funcs[req.path]) ;
 								delete subscribe_funcs[req.path] ;
 							}
 							connection.sendUTF(JSON.stringify({success:true,tid:req.tid}));
-		        		} else {
-		        			almightyClientInterface.callproc(req).then(re=>{
+						} else {
+							pi.client.callProc(req).then(re=>{
 								re.tid = req.tid ;
 								connection.sendUTF(JSON.stringify(re)+"\n") ;
 							}).catch(e=>{
 								e.tid = req.tid ;
 								connection.sendUTF(JSON.stringify(e)+"\n") ;
 							}) ;
-		        		}
-			        } catch(e){
-			        	log('Error in receiving websocket message') ;
-			        	log(JSON.stringify(e)) ;
-			        }
-		        }
+						}
+					} catch(e){
+						log('Error in receiving websocket message') ;
+						log(JSON.stringify(e)) ;
+						log(e);
+					}
+				}
 		        else if (message.type === 'binary') {
 		            log('Received Binary Message of ' + message.binaryData.length + ' bytes. Ignore.');
 		            //connection.sendBytes(message.binaryData);
@@ -165,7 +156,7 @@ exports.init = function(_clientInterface,_globals,_almightyClientInterface){
 		    connection.on('close', function(reasonCode, description) {
 		        //log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
 		        for( let path in subscribe_funcs ){
-			        almightyClientInterface.unsubscribe(path,subscribe_funcs[path]) ;
+			        pi.client.unsubscribe(path,subscribe_funcs[path]) ;
 			    }
 			    subscribe_funcs = {} ;
 		    });
@@ -175,7 +166,7 @@ exports.init = function(_clientInterface,_globals,_almightyClientInterface){
 
 	start_server() ;
 
-	almightyClientInterface.subscribe('/v1/admin/client_settings',re=>{
+	pi.client.subscribe('/v1/admin/client_settings',re=>{
 		let cs = re['/v1/admin/client_settings'] ;
 		if( cs.port != null && cs.port != SERVER_PORT ){
 			localStorage.setItem('PORT_NUM',cs.port) ;
